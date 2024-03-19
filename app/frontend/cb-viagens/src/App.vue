d2b0df9c3889baca75b471601b96e5591ae1ead5
matheus-hrm <rodriguesm673@gmail.com>
2024-03-19 15:58:30 -0300

    set up initial ui

--- app/frontend/cb-viagens/src/App.vue
+++ app/frontend/cb-viagens/src/App.vue
@@ -1,85 +1,25 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { Calculator } from 'lucide-vue-next';
+import CalculatorComponent from './components/CalculatorComponent.vue';
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <div class="flex flex-row w-screen h-screen">
+    <div class="flex flex-col bg-gray-800 w-1/4 h-full">
+      <div class="flex justify-center items-center">
+        <img class="mt-8" src="@/assets/logo.png" width="250" height="250" />
+      </div>
+      <div class="mt-12 flex flex-row justify-center space-x-3">
+        <Calculator class="text-white w-8 h-8" />
+        <h1 class="text-white text-2xl flex justify-center">Calculadora de Viagens</h1>
+      </div>
     </div>
-  </header>
-
-  <RouterView />
+    <div class="flex flex-col h-full w-screen bg-slate-50">
+      <div class="shadow-lg h-28 bg-white"></div>
+      <div class="flex mx-12 my-16 ">
+        <CalculatorComponent />
+      </div>
+    </div>
+  </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
---
+++ app/frontend/cb-viagens/src/components/CalculatorComponent.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { TruckIcon, HandCoinsIcon, ChevronRightIcon, CalendarDaysIcon } from 'lucide-vue-next'
+</script>
+
+<template>
+  <div class="w-full h-full flex flex-col rounded-md drop-shadow-2xl bg-white">
+          <div class="bg-slate-800 flex flex-row space-x-4 items-center px-6 py-4 rounded-t-md" >
+            <TruckIcon class="w-10 h-10 text-white" /> 
+            <h1 class="pl-2 text-white text-2xl">Calculadora de Viagem</h1>
+          </div>
+          <div class="flex flex-row justify-center py-28 items-center ">
+            <div class="bg-gray-100 w-1/3 rounded-md">
+              <div class="flex flex-col justify-center items-center space-y-5 ">
+                <div class="flex flex-row items-center justify-center mt-8">
+                  <HandCoinsIcon class="w-10 h-10 text-black" />
+                  <h1 class="text-2xl p-6">
+                    Calcule o valor da viagem
+                  </h1>
+                </div>
+                <div class="w-full flex flex-col ml-20">
+                  <h1 class="text-gray-900">Destino</h1>
+                  <button class="bg-white flex items-center justify-between border border-gray-300 w-3/4 h-12 rounded-md place-content-start">
+                    <h1 class="text-gray-600 ml-4">Selecione o destino</h1>
+                    <ChevronRightIcon class="w-8 mr-2 text-gray-500" />
+                  </button>
+                </div>
+
+                <div class="w-full flex flex-col ml-20 mt-8">
+                  <h1 class="text-gray-900">Data</h1>
+                  <button class="bg-white flex items-center justify-between border border-gray-300 w-3/4 h-12 rounded-md place-content-start">
+                    <h1 class="text-gray-600 ml-4">Selecione uma data</h1>
+                    <CalendarDaysIcon class="w-8 mr-2 text-gray-500" />
+                  </button>
+                </div>
+
+                <div class="w-2/4 m-8 pb-10">
+                  <button class="bg-cyan-500 text-black w-full h-12 rounded-md mt-8">Buscar</button>
+                </div>
+              </div>
+            </div>
+            <div class="flex flex-col justify-center items-center w-1/2 h-full">
+              <h1 class="text-3xl ">Nenhum dado selecionado</h1>
+            </div>
+          </div>
+        </div>
+</template>
+
