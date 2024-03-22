1926bab4451fc4c1c2f75767c5dc1b15372e9089
matheus-hrm <rodriguesm673@gmail.com>
2024-03-22 20:12:34 -0300

    updated bash script

--- app/run.sh
+++ app/run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-echo "Implemente aqui o script para executar a sua solução"
+# Abrindo uma nova instância do terminal e navegando até o diretório 'api'
+gnome-terminal -- bash -c 'cd api; pip install -r requirements.txt; python -m uvicorn main:app --reload --port 3000'
+
+# Navegando até o diretório 'frontend' na instância atual do terminal
+cd frontend
+
+# Instalando as dependências do frontend e executando o servidor de desenvolvimento
+npm install
+npm run dev
