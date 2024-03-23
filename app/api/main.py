6df91b2da6e9b0792a9ce41434b7a532d22f7cf6
matheus-hrm <rodriguesm673@gmail.com>
2024-03-23 16:19:29 -0300

    Ajuste final da rota

--- app/api/main.py
+++ app/api/main.py
@@ -14,7 +14,34 @@ app.add_middleware(
   
 with open('data.json') as f:
     data = json.load(f)
+    
+def convert_price_to_number(price):
+    return float(price.replace("R$", "").replace(",", "."))
 
+def convert_duration_to_number(duration):
+    return int(duration.replace("h", ""))
+
+def sort_routes_fastest(route):
+    duration = convert_duration_to_number(route["duration"])
+    price_confort = -convert_price_to_number(route["price_confort"])  # negativo para classificar em ordem decrescente
+    return (duration, price_confort)
+
+def get_fastest_route(routes):
+    if routes:
+        return min(routes, key=sort_routes_fastest)
+    else:
+        return None
+
+def sort_routes_cheapest(route):
+    price_econ = convert_price_to_number(route["price_econ"])
+    duration = convert_duration_to_number(route["duration"])  # negativo para classificar em ordem decrescente
+    return (price_econ, duration)
+
+def get_cheapest_route(routes):
+    if routes:
+        return min(routes, key=sort_routes_cheapest)
+    else:
+        return None
 @app.get("/")
 def get_root():
     return {}
@@ -23,7 +50,12 @@ def get_root():
 def get_city(city : str):
     city = city if city != "São Paulo" else "SÃ£o Paulo"
     routes = [route for route in data["transport"] if route["city"] == city]
+    cheapest_route = get_cheapest_route(routes)
+    fastest_route = get_fastest_route(routes)
     if routes:
-        return routes
+        if cheapest_route == fastest_route:
+            return {"cheapest_route": cheapest_route, "fastest_route": "Same as cheapest"}
+        else:
+            return {"cheapest_route": cheapest_route, "fastest_route": fastest_route}
     else:
         raise HTTPException(status_code=404, detail=f"City {city} not found")
